360a5ab7425c12a30b0a599307654555e6eece2d
stakagi <nextplanehome1988@yahoo.co.jp>
2025-08-03 14:18:56 +0300

    Aligns the activity names

--- client/src/components/TrainingDetails.tsx
+++ client/src/components/TrainingDetails.tsx
@@ -40,8 +40,8 @@ const TrainingDetails: React.FC<TrainingDetailsProps> = ({ trainingDay }) => {
                   return <SingleLimbExerciseDetails activity={activity} />;
 
                 case "Power_Clean":
-                case "Clean_and_Jerk":
-                case "Hang_Clean":
+                case "Power_Clean_and_Jerk":
+                case "Power_Hang_Clean":
                   return <WeightTrainingDetails activity={activity} />;
 
                 case "Pogo_Skips":
