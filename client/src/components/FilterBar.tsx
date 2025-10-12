8baf3bc5c9935dcf98384adf5f32ef4abddc3f88
stakagi <nextplanehome1988@yahoo.co.jp>
2025-10-12 17:49:10 +0300

    Replaces the text input field for Select_Activity with a dropdown menu

--- client/src/components/FilterBar.tsx
+++ client/src/components/FilterBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { activityOptions } from "../constants/activityOptions";
 
 interface FilterBarProps {
   activityName: string;
@@ -20,12 +21,18 @@ const FilterBar: React.FC<FilterBarProps> = ({
     <div style={{ marginBottom: "10px" }}>
       <label>
         {t("Activity_Name")}:
-        <input
-          type="text"
+        <select
           value={activityName}
           onChange={(e) => onActivityNameChange(e.target.value)}
           style={{ marginLeft: "10px" }}
-        />
+        >
+          <option value="">{t("Select_Activity")}</option>
+          {activityOptions.map((activity) => (
+            <option key={activity} value={activity}>
+              {t(activity)}
+            </option>
+          ))}
+        </select>
       </label>
 
       <button onClick={onFilter} style={{ marginLeft: "10px" }}>
